8de417daa9bd23bade28260caff88434238fb443
dherrada <dylan.herrada@adafruit.com>
2022-11-18 13:05:20 -0500

    Added commented out board.STEMMA_I2C with explanation

--- examples/ahtx0_simpletest.py
+++ examples/ahtx0_simpletest.py
@@ -11,6 +11,7 @@ import adafruit_ahtx0
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_ahtx0.AHTx0(i2c)
 
 while True:
